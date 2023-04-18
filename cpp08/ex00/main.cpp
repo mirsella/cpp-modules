c20dc6126c748d049ff7efde9080b3497806ba48
mirsella <mirsella@protonmail.com>
2023-04-18 17:52:53 +0200

    add assigmnent test for cpp08/ex01

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,15 +6,14 @@
 /*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 12:43:38 by mirsella          #+#    #+#             */
-/*   Updated: 2023/04/17 15:05:30 by lgillard         ###   ########.fr       */
+/*   Updated: 2023/04/18 13:52:17 by mirsella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 #include <iostream>
 
-int main ()
-{
+int main () {
 	std::vector<int> v;
 
 	for (int i = 0; i < 10; i++)
@@ -25,8 +24,7 @@ int main ()
 		std::cout << *easyfind(v, 9) << std::endl;
 		std::cout << *easyfind(v, 0) << std::endl;
 		std::cout << *easyfind(v, 10) << std::endl;
-	}
-	catch (std::exception &e) {
+	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
 }
--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 12:43:38 by mirsella          #+#    #+#             */
-/*   Updated: 2023/04/17 17:29:58 by lgillard         ###   ########.fr       */
+/*   Updated: 2023/04/18 17:52:10 by mirsella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,19 @@
 
 int main ()
 {
-	try {
-		Span sp = Span(5);
-		sp.addNumber(6);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
-		std::cout << "shortest span: " << sp.shortestSpan() << std::endl;
-		std::cout << "longest span : " << sp.longestSpan() << std::endl;
-	} catch (std::exception& e) {
-		std::cerr << e.what() << std::endl;
-	}
+	Span sp = Span(6);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << "shortest span: " << sp.shortestSpan() << std::endl;
+	std::cout << "longest span : " << sp.longestSpan() << std::endl;
+
+	Span s1 = sp;
+	s1.addNumber(100);
+	sp = s1;
+	std::cout << "longest span is now: " << sp.longestSpan() << std::endl;
 
 
 	std::vector<int> v(10000);
